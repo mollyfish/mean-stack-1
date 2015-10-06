bbf03aed4b8aff95edf6053efc9d205a4de273a5
Molly Kent <kent.molly@gmail.com>
2015-10-06 08:14:42 -0700

    Add 404 response to server.js

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.sendFile('public/index.html', {root: __dirname + '/'});
+  res.status(200).sendFile('public/index.html', {root: __dirname + '/'});
+});
+app.get('/index', function(req, res){
+  res.status(200).sendFile('public/index.html', {root: __dirname + '/'});
+});
+app.get('/index.html', function(req, res){
+  res.status(200).sendFile('public/index.html', {root: __dirname + '/'});
+});
+app.get('/*', function(req, res){
+  res.status(404).sendFile('public/404.html', {root: __dirname + '/'});
 });
 
 var port = process.env.PORT || 3000;
@@ -12,5 +21,3 @@ var server = app.listen(port, 'localhost', function() {
   var port = server.address().port;
   console.log('Server running on: http://localhost:%s',port);
 });
-
-
