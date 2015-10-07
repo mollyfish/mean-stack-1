69a81e9bcbaa8be1adeba1046cfc28606393929b
Molly Kent <kent.molly@gmail.com>
2015-10-07 10:40:21 -0700

    Working webpack, but not in watch mode

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,23 @@
-
 var gulp = require("gulp");
-
 var sass = require("gulp-sass");
+var webpack = require('webpack-stream');
 
 gulp.task("sass", function() {
-    gulp.src("public/sass/**/*.scss")
-        .pipe(sass().on("error", sass.logError))
-        .pipe(gulp.dest("./public/css/"));
+  gulp.src("./public/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./public/css"));
 });
 
-gulp.task("watch", ["sass"], function() {
-  gulp.watch("public/sass/**/*.scss", ["sass"]);
+gulp.task("webpack", function(callback) {
+  return gulp.src("./public/js/index.js")
+  .pipe(webpack({
+    output: {
+      filename: "packed.js"
+    }
+  }))
+  .pipe(gulp.dest('public/js/'));
+});
+
+gulp.task("default", ["sass"], function() {
+  gulp.watch("/public/sass/**/*.scss", ["sass"]);
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,17 @@
-// var app = angular.module("CRUDblog",[]);
+'use strict';
+var angular = require('angular');
+var app = angular.module('CRUDblog', []);
 
-// function MainCtrl ($scope) {
-//   $scope.greeting = "world!";
-// }
-
-// app.controller("MainCtrl", MainCtrl);
+app.controller('Controller', ['$scope', function($scope) {
+  $scope.naomi = { name: 'Naomi', address: '1600 Amphitheatre' };
+  $scope.igor = { name: 'Igor', address: '123 Somewhere' };
+}])
+app.directive('myCustomer', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      customerInfo: '=info'
+    },
+    templateUrl: 'my-customer-iso.html'
+  };
+});
