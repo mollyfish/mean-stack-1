f259819aae2a61ef45740314fbdbf13692a5e1d3
Molly Kent <kent.molly@gmail.com>
2015-10-07 13:46:56 -0700

    Reorg files. Make src directory. Make Previews component

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,13 +3,13 @@ var sass = require("gulp-sass");
 var webpack = require('webpack-stream');
 
 gulp.task("sass", function() {
-  gulp.src("./public/sass/**/*.scss")
+  gulp.src("./src/sass/**/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("./public/css"));
 });
 
 gulp.task("webpack", function(callback) {
-  return gulp.src("./public/js/index.js")
+  return gulp.src("./src/js/index.js")
   .pipe(webpack({
     output: {
       filename: "packed.js"
@@ -19,6 +19,6 @@ gulp.task("webpack", function(callback) {
 });
 
 gulp.task("default", ["webpack", "sass"], function() {
-  gulp.watch("./public/sass/**/*.scss", ["sass"]);
-  gulp.watch("./public/js/**/*.js", ["webpack"]);
+  gulp.watch("./src/sass/**/*.scss", ["sass"]);
+  gulp.watch("./src/js/**/*.js", ["webpack"]);
 });
--- public/js/packed.js
+++ public/js/packed.js
@@ -48,17 +48,16 @@
 	var angular = __webpack_require__(1);
 	var app = angular.module('CRUDblog', []);
 
-	app.controller('Controller', ['$scope', function($scope) {
-	  $scope.zach = { name: 'Zach', address: '1600 Amphitheatre' };
-	  $scope.molly = { name: 'Molly', address: '123 Somewhere' };
+	app.controller('PreviewCtrl', ['$scope', function($scope) {
+	  $scope.post = { title: 'Integer dictum erat et', body: 'The secular cooling that must someday overtake our planet has already gone far indeed with our neighbour. Its physical condition is still largely a mystery, but we know now that even in its equatorial region the midday temperature barely approaches that of our coldest winter. Its air is much more attenuated than ours its oceans have shrunk until they cover but a third of its surface, and as its slow seasons change huge snowcaps gather and melt about either pole and periodically inundate its temperate zones. That last stage of exhaustion, which to us is still incredibly remote, has become a present-day problem for the inhabitants of Mars. The immediate pressure of necessity has brightened their intellects, enlarged their powers, and hardened their hearts. And looking across space with instru- ments, and intelligences such as we have scarcely dreamed of, they see, at its nearest distance only 35,000,000 of miles sunward of them, a morning star of hope, our own warmer planet, green with vegetation and grey with water, with a cloudy atmosphere eloquent of fertility, with glimpses through its drifting cloud wisps of broad stretches of populous country and narrow, navy-crowded seas.', author: 'carlo', date: 'April 7, 2014', comments: '4' };
 	}])
-	app.directive('myCustomer', function() {
+	app.directive('postPreview', function() {
 	  return {
 	    restrict: 'E',
 	    scope: {
-	      customerInfo: '=info'
+	      postInfo: '=info'
 	    },
-	    templateUrl: 'my-customer-iso.html'
+	    templateUrl: 'post-preview.html'
 	  };
 	});
 
